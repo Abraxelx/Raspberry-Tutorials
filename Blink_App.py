283465621f2916644a9f9b244e012d6d6da2bf72
Halil Şahin <xelilim@hotmail.com>
2018-01-12 21:01:04 +0300

    Update Blink_App.py

--- Blink_App.py
+++ Blink_App.py
@@ -1,13 +1,13 @@
-import time
-import RPi.GPIO as GPIO
+import time ##Time Kütüphanesi eklendi
+import RPi.GPIO as GPIO ##Raspberry'nin GPIO Kütüphanesi eklendi ismi GPIO larak tanımlandı
 
 GPIO.setmode(GPIO.BCM)
-LED = 4
-GPIO.setup(LED,GPIO.OUT)
-for counter in range(0,100):
-    GPIO.output(LED,True)
+LED = 4 ## Değeri 4 olan bir LED değişkeni tanımlandı
+GPIO.setup(LED,GPIO.OUT) ##LED Output olarak tanımlandı
+for counter in range(0,100): 
+    GPIO.output(LED,True) ##4'e 5V ver
     print("LED ON")
     time.sleep(3)
-    GPIO.output(4,False)
+    GPIO.output(4,False) ##4'e 0V ver
     print("LED OFF")
     time.sleep(1)
